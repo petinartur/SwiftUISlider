0bee7ed77c121d258155c4df8ebb51805ac01683
Artur <petin.art@gmail.com>
2021-06-07 09:23:00 +0300

    Initial Commit

---
+++ SwiftUISlider/SwiftUISliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISliderApp.swift
+//  SwiftUISlider
+//
+//  Created by Артур Петин on 07.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
